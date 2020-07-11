2a2d6397bbf5cee731194c88d87e0ed4e25d0653
Manasa <raj12ju@gmail.com>
2020-07-11 17:59:43 +0530

    cycle

--- sketch.js
+++ sketch.js
@@ -1,9 +1,68 @@
+var start=2;
+var player;
+var ground;
+var PLAY=1;
+var END=0;
+var gameState=PLAY;
+var lives=2;
+
+function preload(){
+  playerImg=loadAnimation("sprites/tile000.png");
+  coinImg=loadImage("sprites/coin.png");
+}
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+ player= createSprite(200, 320, 50, 50);
+ player.addAnimation("cycing",playerImg);
+ player.scale=0.2;
+ ground=createSprite(200,360,800,20);
+ 
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);  
+  if(gameState===1){
+    ground.velocityX=-3;
+    if(ground.x<0){
+      ground.x=400;
+    }
+    if(keyIsDown(32)){
+      player.velocityY=-10;
+     
+    }
+    player.velocityY=player.velocityY+2;
+    obstacles();
+    coin();
+  }else{
+    ground.velocityX=0;
+
+  }
+
+  
+  
+ 
+  player.collide(ground);
+  console.log(player.y)
+ 
   drawSprites();
+}
+function obstacles(){
+  if(frameCount%60===0){
+  var obstacle=createSprite(800,320,30,30);
+  obstacle.shapeColor="red";
+  obstacle.velocityX=-2;
+
+  }
+}
+function coin(){
+  if(frameCount%200===0){
+    var coin=createSprite(800,230,15,15);
+    coin.velocityX=-2;
+    coin.shapeColor="gold";
+    coin.addImage(coinImg);
+    coin.scale=0.01;
+  
+  }
 }
