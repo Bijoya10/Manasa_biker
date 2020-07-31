5c18140bc184256e3b1a1235b6abf89e1631f3c2
bijoya <bijoya10101@gmail.com>
2020-07-31 08:10:03 +0530

    biker animation

--- sketch.js
+++ sketch.js
@@ -1,61 +1,69 @@
-var start=2;
-var player;
-var ground;
 var PLAY=1;
 var END=0;
 var gameState=PLAY;
-var lives=2;
-var obstaclesGroup;
-var coinGroup;
+var start=2;
+var player;
+var ground;
+var obstaclesGroup,coinGroup;
 var score=0;
 var win;
 var bg,ghost;
+var flag=0;
 
 function preload(){
-  playerImg=loadAnimation("sprites/tile000.png");
+  playerImg=loadAnimation("sprites/biker1.png","sprites/biker2.png","sprites/biker3.png");
+  player_stop=loadAnimation("sprites/biker_stop.png");
   coinImg=loadImage("sprites/coin.png");
-bg=loadImage("sprites/bg1.jpg");
-ghost=loadImage("sprites/ghost.png");
+  bg=loadImage("sprites/bg1.jpg");
+  ghost=loadImage("sprites/ghost.png");
 }
 
 function setup() {
   createCanvas(800,400);
- player= createSprite(200, 280, 50, 50);
- player.addAnimation("cycing",playerImg);
- player.scale=0.2;
- win=createSprite(600,300,10,10);
- win.visible=false;
- back=createSprite(width/2,height/2,width,height);
- back.addImage(bg);
- back.scale=1.8;
- ground=createSprite(400,320,800,20);
- ground.visible=false;
- obstaclesGroup=new Group();
+
+  back=createSprite(width/2,height/2,width,height);
+  back.addImage(bg);
+  back.scale=1.8;
+
+  player= createSprite(200, 280, 50, 50);
+  player.addAnimation("cycing",playerImg);
+  player.addAnimation("player",player_stop);
+  player.scale=0.4;
+
+  win=createSprite(600,300,10,10);
+  win.visible=false;
+  
+  ground=createSprite(400,320,800,20);
+  ground.visible=false;
+
+  obstaclesGroup=new Group();
   coinGroup=new Group();
-  textSize(30);
+
+  textSize(40);
+  textFont("Georgia");
+  fill(255);
  
 }
 
 function draw() {
   background(0);  
-  text("Score: "+score,600,50);
-  player.depth=back.depth+1
-  win.depth=back.depth+1;
+  
+  
   if(gameState===1){
     back.velocityX=-7;
     
-    
-    if(back.x<0){
+    if(back.x<80){
       back.x=back.width/2;
     }
    
-    if(keyIsDown(32)&& player.y>=270.5){
+    if(keyIsDown(32) && player.y>=254.8){
       player.velocityY=-30;
-     
     }
+
     player.velocityY=player.velocityY+2;
     obstacles();
     coin();
+    
     if(player.isTouching(coinGroup)){
       score=score+1;
       coinGroup.destroyEach();
@@ -63,21 +71,19 @@ function draw() {
     if(score===1){
       gameState=2;
     }
-if(player.isTouching(obstaclesGroup)){
-  gameState=0;
-}
+    if(player.isTouching(obstaclesGroup)){
+      gameState=0;
+    }
   }else if(gameState===0){ 
     back.velocityX=0;
-   obstaclesGroup.setVelocityXEach(0);
-   coinGroup.setVelocityXEach(0);
-   obstaclesGroup.setLifetimeEach(-3);
-   coinGroup.setLifetimeEach(-3);
-   player.velocityY=0;
-   textSize(50);
-      textFont("Georgia");
-      fill(255);
-
-      text("You WIN",400,200)
+    obstaclesGroup.setVelocityXEach(0);
+    coinGroup.setVelocityXEach(0);
+    obstaclesGroup.setLifetimeEach(-3);
+    coinGroup.setLifetimeEach(-3);
+    player.velocityY=0;
+    player.changeAnimation("player",player_stop);
+    
+    flag=2;
    
   }else if(gameState===2){
     player.velocityY=player.velocityY+2;
@@ -85,33 +91,34 @@ if(player.isTouching(obstaclesGroup)){
     win.velocityX=-7 
     back.velocityX=-7;
     
-    
-      if(back.x<0){
-        back.x=back.width/2;
-      }
+    if(back.x<0){
+      back.x=back.width/2;
+    }
 
     if(player.isTouching(win)){
       back.velocityX=0;
       win.velocityX=0
       obstaclesGroup.destroyEach();
       coinGroup.destroyEach();
-      textSize(50);
-      textFont("Georgia");
-      fill(255);
-
-      text("You WIN",400,200)
-     
+      player.changeAnimation("player",player_stop);
+      flag=1;
     }
   }
-
-  
-  
  
   player.collide(ground);
-  console.log(player.y)
+  console.log(player.y);
+  drawSprites(); 
+
+  if(flag===1){
+    text("You WIN",400,200)
+  }else if(flag===2){
+    text("You Lose",400,200);
+  }
+
+  text("Score: "+score,600,50);
  
-  drawSprites();
 }
+
 function obstacles(){
   if(frameCount%100===0){
   var obstacle=createSprite(800,280,30,30);
@@ -121,18 +128,17 @@ function obstacles(){
   obstacle.velocityX=-15;
   obstacle.lifetime=400;
   obstaclesGroup.add(obstacle);
-
   }
 }
+
 function coin(){
   if(frameCount%200===0){
     var coin=createSprite(800,200,15,15);
     coin.velocityX=-5;
     coin.shapeColor="gold";
     coin.addImage(coinImg);
-    coin.scale=0.01;
+    coin.scale=0.5;
     coin.lifetime=400;
     coinGroup.add(coin);
-  
   }
 }
